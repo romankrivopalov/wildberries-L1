8d5be0d5861a153cf339007d6cc3e0b76b9049a1
Work desktop <the.red.lemon@yandex.ru>
2023-08-31 13:02:13 +0300

    feat: adds a solution to the twelfth task

---
+++ 12/12.js
@@ -0,0 +1,47 @@
+const book = {
+  // свойства объекта
+  title: 'Fifty Shades',
+  author: 'E. L. James',
+
+  // методы объекта, функции устанавливающие значения ключей
+  setTitle: function(title) {
+    if (title.length < 3) {
+      alert('the title is too short');
+      return
+    };
+    this.title = title;
+  },
+  setAuthor: function(author) {
+    this.author = author;
+  },
+
+  // сеттер и геттер
+  set published(value) {
+    if (typeof value !== "number") {
+      alert('pass the number');
+      return
+    };
+
+    this._published = value;
+  },
+  get published() {
+    return this._published;
+  },
+
+  // получить всю информацию
+  getAllData: function() {
+    return `${this.title}, ${this.author}, ${this._published}`;
+  },
+}
+
+// вызываем метод объекта, чтобы получить всю информацию
+console.log(book.getAllData()); // Fifty Shades of Grey, E. L. James, undefined
+
+// обращение к геттеру и сеттеру (акцессоры) производится без вызова, как метода
+book.published = 2012
+console.log(book.published); // 2012
+
+book.setTitle('Fifty Shades of Grey');
+
+console.log(book.getAllData()); // Fifty Shades of Grey, E. L. James, 2012
+
