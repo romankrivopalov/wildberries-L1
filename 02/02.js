a5c642b438db89f2c04d5cd0add936aee10e1cc3
Home <home@gmail.com>
2023-09-03 14:41:30 +0300

    feat: adds solution for search Perfect number

--- 02/02.js
+++ 02/02.js
@@ -1,3 +1,9 @@
+// Задача о странных числах: Напишите функцию, которая принимает число и возвращает `true`,
+// если это число является странным, и `false` в противном случае.
+// Странным числом считается число, которое равно сумме всех своих делителей, кроме самого себя.
+
+// ==================================================
+
 'use strcit'
 
 const isWeirdNumber = (num) => {
@@ -23,3 +29,25 @@ console.log(isWeirdNumber(16)); // false
 console.log(isWeirdNumber(70)); // true
 console.log(isWeirdNumber(91)); // false
 console.log(isWeirdNumber(836)); // true
+
+// UPD
+  // вариант решения, для поиска не "странного числа", а "совершенного числа"
+  // Соверше́нное число — натуральное число, равное сумме всех своих собственных делителей
+
+  const isPerfectNumber = (num) => {
+    // требуется найти все делители числа, получить их сумму
+    // и сравнить с числом, если сумма равна числу, возвращаем true
+    let sumDivisors = 0;
+
+    for (let i = 1; i < num; i++) {
+      if (num % i === 0) sumDivisors += i;
+    }
+
+    return sumDivisors === num ? true : false;
+  }
+
+  console.log(isPerfectNumber(6)); // true
+  console.log(isPerfectNumber(16)); // false
+  console.log(isPerfectNumber(70)); // false
+  console.log(isPerfectNumber(496)); // true
+  console.log(isPerfectNumber(836)); // false
