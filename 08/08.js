0b390514da770665fd8daff1fee15af5ed3800b0
Home <home@gmail.com>
2023-09-03 14:27:05 +0300

    refactor: update file

--- 08/08.js
+++ 08/08.js
@@ -6,68 +6,68 @@
 
 // два варианта решения, для сихнонных и асинхронных функций
 // Вариант №1, массив синхронных функций
-  const arrayFunc = [
-    funcA = () => {
-      // каждая функция возвращает строку
-      return "Hello! I'm Func A";
-    },
-    funcB = () => {
-      return "Hello! I'm Func B";
-    },
-    funcB = () => {
-      return "Hello! I'm Func C";
-    },
-  ]
+const arrayFunc = [
+  funcA = () => {
+    // каждая функция возвращает строку
+    return "Hello! I'm Func A";
+  },
+  funcB = () => {
+    return "Hello! I'm Func B";
+  },
+  funcB = () => {
+    return "Hello! I'm Func C";
+  },
+]
 
-  const callAllFunc = (array) => {
-    // согласно условию, возвращаем самовызывающуюся функцию,
-    // которая сработает сразу после вызова callAllFunc
-    return function() {
-      // с помощью метода map перебираем каждый элемент массива,
-      // тк мы знаем, что внутри функция, сразу вызываем её
-      // и новый массив, с результатом выполнения каждой функции присваиевам в новую переменную
-      const result = array.map(func => func());
+const callAllFunc = (array) => {
+  // согласно условию, возвращаем самовызывающуюся функцию,
+  // которая сработает сразу после вызова callAllFunc
+  return function() {
+    // с помощью метода map перебираем каждый элемент массива,
+    // тк мы знаем, что внутри функция, сразу вызываем её
+    // и новый массив, с результатом выполнения каждой функции присваиевам в новую переменную
+    const result = array.map(func => func());
 
-      return result;
-    }();
-  }
+    return result;
+  }();
+}
 
-  console.log(callAllFunc(arrayFunc)); // => [ "Hello! I'm Func A", "Hello! I'm Func B", "Hello! I'm Func C" ]
+console.log(callAllFunc(arrayFunc)); // => [ "Hello! I'm Func A", "Hello! I'm Func B", "Hello! I'm Func C" ]
 
 // Вариант №2, массив асинхронных функций
-  const arrayAsyncFunc = [
-    funcA = () => {
-      // каждая функция возвращает промис, для имитации асинхронного кода
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve("Hello! I'm Func A")
-        }, 2000)
-      })
-    },
-    funcB = () => {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve("Hello! I'm Func B")
-        }, 300)
-      })
-    },
-    funcB = () => {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve("Hello! I'm Func C")
-        }, 1000)
-      })
-    },
-  ]
+const arrayAsyncFunc = [
+  funcA = () => {
+    // каждая функция возвращает промис, для имитации асинхронного кода
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve("Hello! I'm Func A")
+      }, 2000)
+    })
+  },
+  funcB = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve("Hello! I'm Func B")
+      }, 300)
+    })
+  },
+  funcB = () => {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve("Hello! I'm Func C")
+      }, 1000)
+    })
+  },
+]
 
-  const callAllAsyncFunc = (array) => {
-    return async function() {
-      // дожидаемся ответа метода Promise.all, который вернет выполнится,
-      // после выполнения каждого из промисов в массиве, который вернёт метод map
-      const result = await Promise.all(array.map(func => func()));
+const callAllAsyncFunc = (array) => {
+  return async function() {
+    // дожидаемся ответа метода Promise.all, который вернет выполнится,
+    // после выполнения каждого из промисов в массиве, который вернёт метод map
+    const result = await Promise.all(array.map(func => func()));
 
-      return result;
-    }();
-  }
+    return result;
+  }();
+}
 
-  callAllAsyncFunc(arrayAsyncFunc).then(res => console.log(res));
+callAllAsyncFunc(arrayAsyncFunc).then(res => console.log(res));
